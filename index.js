7d4741f5e16ade5e299ee6290b7a6579597c5039
YUERONG HUANG <rowanne.h@hotmail.com>
2024-02-15 22:40:17 +1100

    select elements in javascript

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
 document.addEventListener('DOMContentLoaded', () => {
+    let startBtn = document.querySelector('#start');
+    let resetBtn = document.querySelector('#reset');
+    let grids = document.querySelectorAll('#grid>div');
+    console.log(grids)
+
+    startBtn.addEventListener('click', ()=>{
+        alert('start')
+    })
+
+    resetBtn.addEventListener('click', ()=>{
+        alert('reset')
+    })
+
+    grids.forEach(grid => {
+        grid.addEventListener('click', () => {
+            alert('grid')
+        })
+    })
+    
     
 })
