02b539eada95691a2c92ad5c05761a38c96589dc
Rowanne-H <107967304+Rowanne-H@users.noreply.github.com>
2024-02-22 23:36:48 +1100

    Resethandles (#4)
    * add score function, next round function, reset function, bug: post win/draw eventlistening
    
    * fixed bug with grid event listener still on after game wins or draws, this is a working version of the game
    
    ---------
    
    Co-authored-by: Steven <furnbirds@gmail.com>
    Co-authored-by: FurnBirds <153717973+FurnBirds@users.noreply.github.com>

--- index.js
+++ index.js
@@ -7,28 +7,15 @@ document.addEventListener('DOMContentLoaded', () => {
     let scoreDisplay1 = document.querySelector('#player1-score');
     let scoreDisplay2 = document.querySelector('#player2-score');
 
-    let displayWinnerOrDraw = (player) => {
-        let displayWinner = document.querySelector('#result');
-        displayResult.style.zIndex = '1';
-        if(player) {
-            displayWinner.innerHTML = `${player} wins!!!`
-        } else {
-            displayWinner.innerHTML = `Draw!!!`
-        }     
-    }
-    
-    let addScore = (player) => {
-        let scoreDisplay = scoreDisplay1;
-        if (player === 'playerX') { scoreDisplay = scoreDisplay2}
-        let score = parseInt(scoreDisplay.innerHTML);
-        score+=10;
-        scoreDisplay.innerHTML = score;
-    }
-  
-    let playerMove = ['','','','','','','','','']
+    let Xscore = 0;
+    let Oscore = 0;
+
     const playerX = "X";
     const playerO = "O";
+    let playerMove = ['','','','','','','','',''];
     let currentPlayer = playerX;
+    let currentTurn = 0;
+    let gameOn = true
     let winningCombinations = [
         [0,1,2],
         [3,4,5],
@@ -38,7 +25,7 @@ document.addEventListener('DOMContentLoaded', () => {
         [2,5,8],
         [0,4,8],
         [2,4,6]
-    ]
+    ];
     const winningCheck = function (){
         for (const condition of winningCombinations){
             let [a,b,c] = condition
@@ -47,36 +34,91 @@ document.addEventListener('DOMContentLoaded', () => {
             }
         }
     }
+
     const gridClicked = function(e){
-        let index = e.target.id
+        if(gameOn === false){
+            return
+        }
+        let index = e.target.id;
+        ++currentTurn;
         if(!playerMove[index]){
             playerMove[index] = currentPlayer;
             e.target.innerText = currentPlayer;
             if(winningCheck() == true){
-                addScore(currentPlayer) 
-                setTimeout(() => displayWinnerOrDraw(currentPlayer), 100);   
+
+                //alert(`${currentPlayer} has won`);
+                gameOn = false;
+                displayWinnerFx(currentPlayer);
+                addScore(currentPlayer);
+                return;   
+                } else{ //check for a draw if winner not achieved.
+                    if(currentTurn === 9){
+                        gameOn = false;
+                        displayDraw()
+                    }
+                }
+        currentPlayer = currentPlayer === playerX ? playerO : playerX;
         }
-        setTimeout(() => {currentPlayer = currentPlayer === playerX ? playerO : playerX}, 100)
-        }
-        if(playerMove.includes('')===false) {
-            setTimeout(() => displayWinnerOrDraw(), 100);  
-        }
-    }
+    };
     
 
-    startBtn.addEventListener('click', ()=>{
-        alert('start')
-    })
+    const displayWinnerFx = (player) => {
+        let displayWinner = document.querySelector('#winner');
+        gameOn = false;
+        displayWinner.style.visibility = 'visible'
+        if(player) {
+            displayWinner.innerHTML = `${player} wins!!! Press NEXT ROUND to Start again`
+        } else {
+            displayWinner.innerHTML = `Draw!!! Press NEXT ROUND to Start again`
+        }
+    };
 
-    resetBtn.addEventListener('click', ()=>{
-        alert('reset')
-    })
+    const displayDraw = () => {
+        gameOn = false;
+        let displayWinner = document.querySelector('#winner');
+        displayWinner.style.visibility = 'visible'
+        displayWinner.innerHTML = `Draw!!! Press NEXT ROUND to Start again`
 
-    playAgainBtn.addEventListener('click', ()=>{
-        alert('playAgain')
-    })
+    };
+    
+   //we keep the score in Xscore and OScore instead of the
+   //scoreDisplay1, scoreDisplay2 html element
+
+    const addScore = (player) => {
+        if(player === 'X'){
+            scoreDisplay1.innerHTML = ++Xscore
+        } else{
+            scoreDisplay2.innerHTML = ++Oscore
+        }
+    };
+
+    
+    const restartGame = function() {
+        gameOn = true;
+        let hideDisplay = document.querySelector('#winner');
+        hideDisplay.style.visibility = 'hidden';
+        playerMove = ['','','','','','','','',''];
+        currentTurn = 0;
+        console.log(playerMove);
+        grids.forEach(grid => {
+            grid.innerText = "";
+        });
+    }
+    
+    const resetGame = function() {
+        Oscore = 0;
+        scoreDisplay1.innerHTML = Oscore;
+        Xscore = 0;
+        scoreDisplay2.innerHTML = Xscore;
+        restartGame()
+    }
+
+    startBtn.addEventListener('click', restartGame)
+
+    resetBtn.addEventListener('click', resetGame)
+
     grids.forEach(grid => {
-      grid.addEventListener('click', gridClicked, {once: true})
+      grid.addEventListener('click', gridClicked) //, {once: true}) <- this prevents grid listening to click after restart 
     })
 })
 
