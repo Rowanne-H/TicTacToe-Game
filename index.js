7f8cc4fbe0b75f7554819c476338755089319b35
FurnBirds <153717973+FurnBirds@users.noreply.github.com>
2024-02-27 16:22:33 +0800

    Steve edit (#5)
    * add two buttons to change mode between  player 1 VS player 2 or player 1 VS computer
    
    * put change currentPlayer to X or O inside function switchPlayer
    
    * edit js code to choose between 2 modes and display computer in page when player 1 vs computer has been chosen
    
    * put addScore inside function displayWinnerFx and add one condition in gridClicked function to let computer take turn
    
    * create condition when there are two X and place O in the index of  in computerTurn function
    
    * create condition when there are two O and place O in the index of "" in computerTurn function
    
    * crete one condition in restartGame function when it is computer turn to place O
    
    * if there is no O, place O in the middle or if middle has X, place O in the first index of
    
    * add code to check whether it is draw after computer place O
    
    * delete checkAgain condition for there are 2 X and place O
    
    * create a condition to let computer place O randomly so that player 1 have a chance to win
    
    * add instructions to READEME.md
    
    * fix error no checking winning condition aafter computer place O randomly
    
    * delete console.log
    
    * change title TicTacToe-Game font size
    
    * add instructions for NEXT ROUND button and labels for players and scores
    
    * fix error no putting space after ###
    
    * rewrite description
    
    * update the rule of the game
    
    * changed visibility of grid, buttons to appear/disappear to game's progress, rest game now also allows changing game mode between vs player 2 or vs computer
    
    ---------
    
    Co-authored-by: YUERONG HUANG <rowanne.h@hotmail.com>
    Co-authored-by: Thuy Duong Nguyen <thuyduong.nguyen.091096@gmail.com>

--- index.js
+++ index.js
@@ -5,12 +5,20 @@ document.addEventListener('DOMContentLoaded', () => {
     let scoreDisplay1 = document.querySelector('#player1-score');
     let scoreDisplay2 = document.querySelector('#player2-score');
     let displayWinner = document.querySelector('#winner');
+    let onePlayerBtn = document.querySelector('#one-player');
+    let twoPlayerBtn = document.querySelector('#two-player');
+    let player1 = document.querySelector('#player1');
+    let player2 = document.querySelector('#player2');
+    let playMode = document.getElementById('playMode');
+    let thegrid = document.getElementById('grid')
+    let gameOn = false;
+    let computer = false;
+    let sameGame = true;
 
     let Xscore = 0;
     let Oscore = 0;
     let playerMove = ['', '', '', '', '', '', '', '', ''];
     let currentPlayer = "X";
-    let gameOn = true
     let winningCombinations = [
         [0, 1, 2],
         [3, 4, 5],
@@ -31,49 +39,6 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     }
 
-    const switchPlayer = (player) => {
-        if (currentPlayer === 'X') {
-            player1.className = 'current-player';
-            player2.className = '';
-        } else {
-            player1.className = '';
-            player2.className = 'current-player';
-        }
-    }
-
-    const gridClicked = function (e) {
-        if (gameOn === false) {
-            return
-        }
-        let index = e.target.id;
-        if (!playerMove[index]) {
-            playerMove[index] = currentPlayer;
-            e.target.innerText = currentPlayer;
-            if (winningCheck() === true) {
-                displayWinnerFx(currentPlayer);
-                addScore(currentPlayer);
-                return;
-            } else { //check for a draw if winner not achieved.
-                if (playerMove.includes('') === false) {
-                    displayWinnerFx();
-                    return;
-                }
-            }
-            currentPlayer = currentPlayer === "X" ? "O" : "X";
-            switchPlayer(currentPlayer)
-        }
-    };
-
-    const displayWinnerFx = (player) => {
-        gameOn = false;
-        displayWinner.style.visibility = 'visible';
-        if (player) {
-            displayWinner.innerHTML = `${player} wins!!! Press NEXT ROUND to Start again`
-        } else {
-            displayWinner.innerHTML = `Draw!!! Press NEXT ROUND to Start again`
-        }
-    };
-
     const addScore = (player) => {
         if (player === 'X') {
             scoreDisplay1.innerHTML = ++Xscore
@@ -82,24 +47,220 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     };
 
-    const restartGame = function () {
+    const displayWinnerFx = (player) => {
+        gameOn = false;
+        displayWinner.style.visibility = 'visible';
+        if (player) {
+            displayWinner.innerHTML = `${player} wins!!! Press NEXT ROUND to Start again`
+            addScore(currentPlayer)
+        } else {
+            displayWinner.innerHTML = `Draw!!! Press NEXT ROUND to Start again`
+        }
+    };
+
+    const switchPlayer = (player) => {
+        if (player === 'X') {
+            currentPlayer = 'O';
+            player1.className = '';
+            player2.className = 'current-player';
+        } else {
+            currentPlayer = 'X';
+            player1.className = 'current-player';
+            player2.className = '';
+        }
+    }
+
+    const placeO = (i) => {//please update if there is a simpler way to get element by ID using a variable with index stored in it
+        playerMove[i] = 'O';
+        if (i == 0) {
+            document.getElementById('0').innerHTML = 'O';
+        } else if (i == 1) {
+            document.getElementById('1').innerHTML = 'O';
+        } else if (i == 2) {
+            document.getElementById('2').innerHTML = 'O';
+        } else if (i == 3) {
+            document.getElementById('3').innerHTML = 'O';
+        } else if (i == 4) {
+            document.getElementById('4').innerHTML = 'O';
+        } else if (i == 5) {
+            document.getElementById('5').innerHTML = 'O';
+        } else if (i == 6) {
+            document.getElementById('6').innerHTML = 'O';
+        } else if (i == 7) {
+            document.getElementById('7').innerHTML = 'O';
+        } else {
+            document.getElementById('8').innerHTML = 'O';
+        }
+    }
+
+    const computerTurn = () => {
+        let count;
+        let iToUse;
+        let checkAgain = true;
+        
+        //computer place O randomly so that player 1 have a chance to win
+        if (playerMove.filter(e => e === '').length < 4) {
+            const getRandomI = () => {
+                let i = Math.floor(Math.random() * 9);
+                if (!playerMove[i]) {
+                    return i
+                } else {
+                    return getRandomI()
+                }
+            }
+            iToUse = getRandomI();
+            placeO(iToUse)
+            if (winningCheck() === true) {
+                displayWinnerFx(currentPlayer);
+                return;
+            }
+        } else {
+            //if there are 2 'O', place O in the index of ''
+            for (let i = 0; i < winningCombinations.length; i++) {
+                count = 0;
+                iToUse = -1;
+                for (let j = 0; j < winningCombinations[i].length; j++) {
+                    let index = winningCombinations[i][j];
+                    if (playerMove[index] === 'O') { count++ }
+                    if (playerMove[index] === '') { iToUse = index }
+                }
+                if (count === 2 && iToUse != -1) {
+                    placeO(iToUse);
+                    displayWinnerFx('O');
+                    return;
+                }
+            }
+
+            //if there are 2 'X', place O in the index of ''
+            for (let i = 0; i < winningCombinations.length; i++) {
+                count = 0;
+                iToUse = -1;
+                for (let j = 0; j < winningCombinations[i].length; j++) {
+                    let index = winningCombinations[i][j];
+                    if (playerMove[index] === 'X') { count++ }//count X
+                    if (playerMove[index] === '') { iToUse = index; }//find index of ''
+                }
+                if (count === 2 && iToUse != -1) {
+                    placeO(iToUse)
+                    checkAgain = false;
+                    break;
+                }
+            }
+
+            //if there is no 'O', place O in the middle or if middle has 'X', place O in the first index of ''
+            if (checkAgain === true) {
+                if (playerMove[4] === '') {
+                    iToUse = 4;
+                } else {
+                    iToUse = playerMove.indexOf('');
+                }
+                placeO(iToUse);
+            }
+        }
+
+        if (playerMove.includes('') === false) {
+            displayWinnerFx();
+            return;
+        }
+
         gameOn = true;
+        switchPlayer(currentPlayer);
+    }
+
+    const gridClicked = function (e) {
+        if (gameOn === true) {
+            let index = e.target.id;
+            if (!playerMove[index]) {
+                playerMove[index] = currentPlayer;
+                e.target.innerText = currentPlayer;
+                if (winningCheck() === true) {
+                    displayWinnerFx(currentPlayer);
+                } else if (playerMove.includes('') === false) { //check for a draw if winner not achieved.
+                    displayWinnerFx();
+                } else {
+                    if (computer === 'O') {//check for computer player
+                        gameOn = false;
+                        switchPlayer(currentPlayer);
+                        setTimeout(computerTurn, 500);
+                    } else {
+                        switchPlayer(currentPlayer)
+                    }
+                }
+            }
+        }
+    }
+
+    const restartGame = function () {
+        gameOn = sameGame;
         displayWinner.style.visibility = 'hidden';
         playerMove = ['', '', '', '', '', '', '', '', ''];
         grids.forEach(grid => {
             grid.innerText = "";
         });
+        setTimeout(() => {
+            if (currentPlayer === 'O' && computer === 'O') {
+                placeO(4);
+                switchPlayer(currentPlayer)
+            }
+            gameOn = sameGame;
+        }, 500);
+        return
     }
 
     const resetGame = function () {
+        sameGame = false;
+        computer = false;
         Oscore = 0;
         scoreDisplay1.innerHTML = Oscore;
         Xscore = 0;
         scoreDisplay2.innerHTML = Xscore;
-        currentPlayer = "X";
-        switchPlayer(currentPlayer);
+        onePlayerBtn.style.color = '';
+        twoPlayerBtn.style.color = '';
+        player1.className = 'player';
+        player2.className = 'player';
+        player2.innerText = 'Player 2';
+        currentPlayer = 'X' 
+        onePlayerBtn.style.visibility = 'visible';
+        twoPlayerBtn.style.visibility = 'visible';
+        playMode.innerText = '';
+        startBtn.style.visibility = 'hidden';
+        resetBtn.style.visibility = 'hidden';
+        thegrid.style.visibility = 'hidden'
         restartGame()
     }
+
+    const startGame = function () {
+        onePlayerBtn.style.visibility = 'hidden';
+        twoPlayerBtn.style.visibility = 'hidden';
+        startBtn.style.visibility = 'visible';
+        resetBtn.style.visibility = 'visible';
+        thegrid.style.visibility = 'visible'
+    }
+
+    onePlayerBtn.addEventListener('click', () => {
+        if (gameOn === false && computer === false) {
+            gameOn = true;
+            computer = 'O';
+            sameGame = true;
+            switchPlayer('O');
+            player2.innerText = 'Computer';
+            onePlayerBtn.style.color = 'gray';
+            playMode.innerText = 'Player Vs Computer';
+            startGame()
+        }
+    })
+
+    twoPlayerBtn.addEventListener('click', () => {
+        if (gameOn === false && computer === false) {
+            gameOn = true;
+            sameGame = true;
+            switchPlayer('O');
+            twoPlayerBtn.style.color = 'gray';
+            playMode.innerText = 'Player 1 Vs Player 2';
+            startGame()
+
+        }
+    })
 
     startBtn.addEventListener('click', restartGame)
 
